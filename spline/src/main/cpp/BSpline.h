c36da29b9fc4f8e964644758a916885d1d92d7ee
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2020-04-27 19:50:22 +0300

    code style [2]

--- spline/src/main/cpp/BSpline.h
+++ spline/src/main/cpp/BSpline.h
@@ -3,14 +3,13 @@
 
 #include "Curve.h"
 
-class BSpline : public Curve
-{
+class BSpline : public Curve {
 public:
-	BSpline();
-	virtual ~BSpline();
+    BSpline();
+    virtual ~BSpline();
 
 protected:
-	virtual void _on_way_point_added();
+    virtual void _on_way_point_added();
 
 protected:
     Vector interpolate_3(double u, const Vector& P0, const Vector& P1, const Vector& P2, const Vector& P3);
