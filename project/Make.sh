b9ba20a0f2e5b5d3e94f3079cbe02db248cd2df1
subake <32021623+subake@users.noreply.github.com>
2020-05-15 11:56:41 +0300

    Final version

---
+++ project/Make.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mpiCC spline/main.cpp spline/BSpline/BSpline.cpp spline/Vector/Vector.cpp spline/Curve/Curve.cpp -o a.out
---
+++ project/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install python-pip
+pip install numpy
+pip install matplotlib
+pip install runipy
